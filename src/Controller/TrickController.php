f6869508e70fd3a6cf789d0a05499182c303bda0
btolan-karudev <beniamin.tolan@outlook.com>
2020-03-31 13:56:01 +0200

    test session interfrace   et test photo

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,20 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
 {
     /**
      * @Route("/tricks", name="tricks_index")
+     * @param TrickRepository $trickRepository
+     * @param SessionInterface $session
+     * @return Response
      */
-    public function index()
+    public function index(TrickRepository $trickRepository, SessionInterface $session)
     {
-        $repo = $this->getDoctrine()->getRepository(Trick::class);
+        dump($session);
 
-        $tricks = $repo->findAll();
+        $tricks = $trickRepository->findAll();
 
         return $this->render('trick/index.html.twig', [
             'tricks' => $tricks
