5547c2f8fa082da6c51f54f27fa628cd020a85f0
btolan-karudev <beniamin.tolan@outlook.com>
2020-03-31 13:56:57 +0200

    refactor

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TrickController extends AbstractController
@@ -13,13 +12,10 @@ class TrickController extends AbstractController
     /**
      * @Route("/tricks", name="tricks_index")
      * @param TrickRepository $trickRepository
-     * @param SessionInterface $session
      * @return Response
      */
-    public function index(TrickRepository $trickRepository, SessionInterface $session)
+    public function index(TrickRepository $trickRepository)
     {
-        dump($session);
-
         $tricks = $trickRepository->findAll();
 
         return $this->render('trick/index.html.twig', [
