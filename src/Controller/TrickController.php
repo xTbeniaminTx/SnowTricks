4d4d63d352e1a91ce09d9a99f3057cbfc9ab319b
btolan-karudev <beniamin.tolan@outlook.com>
2020-03-31 18:02:10 +0200

    object manager imported

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Trick;
 use App\Form\TrickType;
 use App\Repository\TrickRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,13 +33,21 @@ class TrickController extends AbstractController
      * Create a trick
      *
      * @Route("/tricks/new", name="triks_create")
-     *
+     * @param Request $request
+     * @return Response
      */
-    public function create()
+    public function create(Request $request, ObjectManager $manager)
     {
         $trick = new Trick();
 
         $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() and $form->isValid()) {
+            $manager->persist($trick);
+            $manager->flush($trick);
+        }
 
         return $this->render('trick/new.html.twig', [
             'form' => $form->createView()
--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -4,18 +4,40 @@ namespace App\Form;
 
 use App\Entity\Trick;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TrickType extends AbstractType
 {
+
+    /**
+     * Basic field configuration !
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder)
+    {
+        return [
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ];
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class,
+                $this->getConfiguration('Titre', 'Veuillez insere un joli titre a votre trick'))
+            ->add('content', TextareaType::class,
+                $this->getConfiguration('Description détaillée', 'Tapez une description qui donne vraiment envie de l\'essayer'))
             ->add('createdAt')
             ->add('modifiedAt')
-            ->add('content')
 //            ->add('category')
 //            ->add('author')
         ;
