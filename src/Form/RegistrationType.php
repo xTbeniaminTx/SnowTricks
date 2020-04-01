129c58cfe0e437b26316a05ae32de4f237bf4f71
btolan-karudev <beniamin.tolan@outlook.com>
2020-04-01 14:21:37 +0200

    registration type

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    /**
+     * Basic field configuration !
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = [])
+    {
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom", "Votre prénom..."))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom de famille..."))
+            ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre adresse email..."))
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "Url de votre avatar..."))
+            ->add('password', PasswordType::class, $this->getConfiguration("Mot de passe", "Choisissez un bon mot de passe..."))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
