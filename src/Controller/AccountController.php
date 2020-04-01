46a3d2cf9b391499a97a68ab7610371e7b2f9081
btolan-karudev <beniamin.tolan@outlook.com>
2020-04-01 13:54:18 +0200

    logout and auth utils on

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,15 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
     /**
+     * View and manage the login form
+     *
      * @Route("/login", name="account_login")
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
-        return $this->render('account/login.html.twig');
+        $error = $utils->getLastAuthenticationError();
+        $lastusername = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'lastusername' => $lastusername
+        ]);
+    }
+
+    /**
+     * Allows to disconnect
+     *
+     * @Route("/logout", name="account_logout")
+     *
+     * @return void
+     */
+    public function logout()
+    {
+
     }
 }
