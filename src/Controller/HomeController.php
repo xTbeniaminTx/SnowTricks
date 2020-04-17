2d1de6a8bfa98bacce0fd28a6d04268d50624047
btolan-karudev <beniamin.tolan@outlook.com>
2020-04-17 16:44:31 +0200

    path to profile

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,10 +4,10 @@
 namespace App\Controller;
 
 
-
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 
 class HomeController extends BaseController
@@ -15,13 +15,16 @@ class HomeController extends BaseController
     /**
      * @Route("/", name="homepage")
      *
+     * @param TrickRepository $trickRepository
      * @return Response
      */
-    public function home()
+    public function home(TrickRepository $trickRepository)
     {
-
+        $tricks = $trickRepository->findAll();
         return $this->render(
-            'home.html.twig'
+            'home.html.twig', [
+                'tricks' => $tricks
+            ]
         );
 
     }
